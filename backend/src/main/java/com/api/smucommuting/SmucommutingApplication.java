77b9c971c6b4a89de6d55a27c9ba3f1149604942
donup3 <donup08@naver.com>
2022-05-09 12:30:18 +0900

    project init

---
+++ backend/src/main/java/com/api/smucommuting/SmucommutingApplication.java
@@ -0,0 +1,13 @@
+package com.api.smucommuting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmucommutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmucommutingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/api/smucommuting/SmucommutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.smucommuting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmucommutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
