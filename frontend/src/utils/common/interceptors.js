b03b773266bd30ccff84a89483b59f9e2b37a444
donup3 <donup08@naver.com>
2022-05-20 13:14:43 +0900

    feat: axios 인터셉터 수정(#8)

--- frontend/src/utils/common/interceptors.js
+++ frontend/src/utils/common/interceptors.js
@@ -21,28 +21,20 @@ export function setInterceptors(instance) {
                 response.data.error.type ===
                 'InsufficientAuthenticationException'
             ) {
-                return axios({
-                    method: 'POST',
-                    url: `${process.env.REACT_APP_API_URL}/api/auth/refresh`,
-                    headers: {
-                        Authorization: `Bearer ${axios.defaults.headers.common.Authorization}`,
-                    },
-                })
+                return axios
+                    .post(`${process.env.REACT_APP_API_URL}/api/auth/refresh`)
+
                     .then(res => {
-                        console.log('1차');
                         axios.defaults.headers.common.Authorization =
                             res.data.data.accessToken;
                     })
                     .then(() => {
-                        console.log('2차');
                         originalRequest.headers.Authorization = `Bearer ${axios.defaults.headers.common.Authorization}`;
                         return axios(originalRequest);
                     })
                     .catch(err => {
-                        // window.location.replace('/');
-                        console.log('에러');
-                        // originalRequest.headers.Authorization = `Bearer ${axios.defaults.headers.common.Authorization}`;
-                        // return axios(originalRequest);
+                        window.location.replace('/');
+                        localStorage.clear();
                     });
             }
             return Promise.reject(err);
