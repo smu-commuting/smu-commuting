a9b33a39260ddb393b29ec9a962f9158592861f2
donup3 <donup08@naver.com>
2022-05-20 13:12:37 +0900

    feat: refresh token 쿠키 생성 및 axios 설정 추가(#8)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
+import axios from 'axios';
 import store from './modules/store/configureStore';
 import App from './pages/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './styles/_reset.scss';
 
+axios.defaults.withCredentials = true;
 const persistor = persistStore(store());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
