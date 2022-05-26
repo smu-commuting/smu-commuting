0c39dfe7ae9a623414cce809349e5d672f353dc9
donup3 <donup08@naver.com>
2022-05-26 17:47:48 +0900

    fix: websocket 설정 수정

--- backend/src/main/java/com/api/smucommuting/taxichat/config/WebSocketConfig.java
+++ backend/src/main/java/com/api/smucommuting/taxichat/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000").withSockJS();
     }
 
     @Override
