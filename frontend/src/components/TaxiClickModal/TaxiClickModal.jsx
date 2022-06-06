403bf76f05f491344d2b57624f494ca523bdde15
colo1211 <colo1211@naver.com>
2022-06-06 21:22:17 +0900

    feat: 택시 파티 조회 모달 날짜 스크롤 구현(#36)

--- frontend/src/components/TaxiClickModal/TaxiClickModal.jsx
+++ frontend/src/components/TaxiClickModal/TaxiClickModal.jsx
@@ -4,9 +4,11 @@ import { useDispatch } from 'react-redux';
 import { taxiModalClick } from '../../modules/reducers/user';
 import cancel from '../../assets/TaxiPage/cancel.png';
 import './TaxiClickModal.scss';
+import downtoggle from '../../assets/TaxiPage/downtoggle.png';
 import { monthDay } from '../../constants';
 
 function TaxiClickModal() {
+    const [yearLists, setYearLists] = useState([]);
     const [year, setYear] = useState();
     const [month, setMonth] = useState();
     const [date, setDate] = useState();
@@ -15,6 +17,11 @@ function TaxiClickModal() {
         setYear(now.getFullYear());
         setMonth(now.getMonth() + 1);
         setDate(now.getDate());
+        setYearLists([
+            now.getFullYear() - 1,
+            now.getFullYear(),
+            now.getFullYear() + 1,
+        ]);
     }, []);
     const dispatch = useDispatch();
     const onCancelClick = useCallback(() => {
@@ -40,14 +47,32 @@ function TaxiClickModal() {
                         <div className="year-box">
                             <p className="box-name">{year}년</p>
                             <div className="year-scroll">
-                                <p className="click-year">{year}년</p>
-                                <p
-                                    className="non-click-year"
-                                    onClick={() => setYear(year + 1)}
-                                    aria-hidden
-                                >
-                                    {year + 1}년
-                                </p>
+                                {yearLists &&
+                                    yearLists.map(yearList => {
+                                        return year === yearList ? (
+                                            <p
+                                                className="click-year"
+                                                key={yearList}
+                                            >
+                                                {yearList}년
+                                            </p>
+                                        ) : (
+                                            <p
+                                                className="non-click-year"
+                                                key={yearList}
+                                                onClick={() => {
+                                                    setYear(yearList);
+                                                    window.scrollTo({
+                                                        top: yearList.offsetTop,
+                                                        behavior: 'smooth',
+                                                    });
+                                                }}
+                                                aria-hidden
+                                            >
+                                                {yearList}년
+                                            </p>
+                                        );
+                                    })}
                             </div>
                         </div>
                         <div className="month-box">
@@ -56,7 +81,13 @@ function TaxiClickModal() {
                                 {month &&
                                     Object.keys(monthDay).map(mon => {
                                         return month === parseInt(mon, 10) ? (
-                                            <p className="click-mon">{mon}월</p>
+                                            <p
+                                                className="click-mon"
+                                                key={mon}
+                                                aria-hidden
+                                            >
+                                                {mon}월
+                                            </p>
                                         ) : (
                                             <p
                                                 className="non-click-mon"
@@ -65,12 +96,9 @@ function TaxiClickModal() {
                                                     setDate(
                                                         new Date().getDate(),
                                                     );
-                                                    window.scrollTo({
-                                                        top: 0,
-                                                        behavior: 'smooth',
-                                                    });
                                                 }}
                                                 aria-hidden
+                                                key={mon}
                                             >
                                                 {mon}월
                                             </p>
@@ -80,22 +108,27 @@ function TaxiClickModal() {
                         </div>
                         <div className="day-box">
                             <p className="box-name">{date}일</p>
-                            {month &&
-                                monthDay[month].map(day => {
-                                    return date === parseInt(day, 10) ? (
-                                        <p className="click-day">{day}</p>
-                                    ) : (
-                                        <p
-                                            className="non-click-day"
-                                            onClick={() =>
-                                                setDate(parseInt(day, 10))
-                                            }
-                                            aria-hidden
-                                        >
-                                            {day}
-                                        </p>
-                                    );
-                                })}
+                            <div className="day-scroll">
+                                {month &&
+                                    monthDay[month].map(day => {
+                                        return date === parseInt(day, 10) ? (
+                                            <p className="click-day" key={day}>
+                                                {day}일
+                                            </p>
+                                        ) : (
+                                            <p
+                                                className="non-click-day"
+                                                onClick={() =>
+                                                    setDate(parseInt(day, 10))
+                                                }
+                                                aria-hidden
+                                                key={day}
+                                            >
+                                                {day}일
+                                            </p>
+                                        );
+                                    })}
+                            </div>
                         </div>
                     </div>
                 </div>
