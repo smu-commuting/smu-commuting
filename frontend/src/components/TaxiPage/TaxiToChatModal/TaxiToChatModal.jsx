7a9ba8ab9c207d69fb4107ed0f83ee8baa1c4b43
colo1211 <colo1211@naver.com>
2022-06-10 04:23:26 +0900

    feat: 택시 모달 클릭시, 정보와 함께 띄우기 완료(#45)

--- frontend/src/components/TaxiPage/TaxiToChatModal/TaxiToChatModal.jsx
+++ frontend/src/components/TaxiPage/TaxiToChatModal/TaxiToChatModal.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { taxiToChatModal } from '../../../modules/reducers/taxi';
 import './TaxiToChatModal.scss';
 
 function TaxiToChatModal() {
+    const dispatch = useDispatch();
     const { chattingRoomInfo } = useSelector(state => state.taxi);
+    useEffect(() => {
+        // 시간 파싱
+    }, []);
+    const onCancelClick = useCallback(() => {
+        dispatch(taxiToChatModal());
+    }, [dispatch]);
+    const onAgreeClick = useCallback(() => {
+        // dispatch(moveToChatRoom());
+    }, [dispatch]);
     return (
         <div className="taxitochatmodal-wrapper">
-            {chattingRoomInfo.placeName}
-            {chattingRoomInfo.taxiPartyId}
-            {chattingRoomInfo.time}
+            <div className="taxitochatmodal">
+                <p>
+                    {chattingRoomInfo.placeName}에서&nbsp;
+                    {chattingRoomInfo.time}에 탑승하는 <br />
+                    모임에 입장하시겠습니까?
+                </p>
+                <div className="button-wrapper">
+                    <div className="cancel" onClick={onCancelClick} aria-hidden>
+                        <p>취소</p>
+                    </div>
+                    <div className="agree" onClick={onAgreeClick} aria-hidden>
+                        <p>확인</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
