8799ad9c2e5255020d4bfdf60218d20750f4a6bc
donup3 <donup08@naver.com>
2022-06-17 16:24:13 +0900

    feat: 채팅 origin 설정 추가

--- backend/src/main/java/com/api/smucommuting/taxichat/config/WebSocketConfig.java
+++ backend/src/main/java/com/api/smucommuting/taxichat/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.api.smucommuting.taxichat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,9 +10,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${dev.server.url}")
+    private String devUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000", devUrl).withSockJS();
     }
 
     @Override
