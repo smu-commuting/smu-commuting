110f68b1817939b552046f2bf86bea64dada7def
colo1211 <colo1211@naver.com>
2022-06-21 19:20:43 +0900

    fix: 소셜 로그인 리다이렉트 시도1(#59)

--- frontend/src/pages/LogInProcess/LogInProcess.jsx
+++ frontend/src/pages/LogInProcess/LogInProcess.jsx
@@ -1,31 +1,36 @@
 /* eslint-disable dot-notation */
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Navigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { loginRequest } from '../../modules/reducers/user';
 import LoadingPage from '../LoadingPage/LoadingPage';
 
 function LogInProcess() {
-    const navigate = useNavigate();
     const dispatch = useDispatch();
     const { id, accessToken, studentId } = useParams();
     useEffect(() => {
         axios.defaults.headers.common['Authorization'] = accessToken;
-        if (studentId === 'null') {
-            navigate('/signup');
-        } else {
+        if (studentId !== 'null') {
             const userInfo = {
                 email: `${studentId}@sangmyung.kr`,
                 studentId,
                 id,
             };
             dispatch(loginRequest(userInfo));
-            navigate('/home');
         }
     }, []);
-    return <LoadingPage />;
+    return (
+        <>
+            <LoadingPage />
+            {studentId !== 'null' ? (
+                <Navigate to="/home" />
+            ) : (
+                <Navigate to="/signup" />
+            )}
+        </>
+    );
 }
 
 export default LogInProcess;
