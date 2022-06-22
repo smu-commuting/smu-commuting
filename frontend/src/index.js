d6da6076e4710633e05807f95a6acb7c56807001
donup3 <donup08@naver.com>
2022-06-22 09:02:21 +0900

    pwa 제거

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import axios from 'axios';
 import configureStore from './modules/store/configureStore';
 import App from './pages/App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './styles/_reset.scss';
 
 axios.defaults.withCredentials = true;
@@ -20,8 +19,3 @@ root.render(
         </PersistGate>
     </Provider>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
