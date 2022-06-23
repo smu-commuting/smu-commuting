f0d94d38103dae648118e5ca00f021733bca5d97
donup3 <donup08@naver.com>
2022-06-23 17:08:26 +0900

    feat: 알림 test api 추가(#47)

---
+++ backend/src/main/java/com/api/smucommuting/common/notification/NoticeTestDto.java
@@ -0,0 +1,10 @@
+package com.api.smucommuting.common.notification;
+
+import lombok.Data;
+
+@Data
+public class NoticeTestDto {
+    private String token;
+    private String title;
+    private String body;
+}
