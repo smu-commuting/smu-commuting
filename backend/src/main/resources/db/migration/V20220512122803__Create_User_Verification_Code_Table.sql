e330930ac87f531e46b558a8e5391babd17cf0e7
donup3 <donup08@naver.com>
2022-07-13 19:51:05 +0900

    feat: fcm token 저장 데이터 크기 수정

--- backend/src/main/resources/db/migration/V20220512122803__Create_User_Verification_Code_Table.sql
+++ backend/src/main/resources/db/migration/V20220512122803__Create_User_Verification_Code_Table.sql
@@ -5,7 +5,7 @@ CREATE TABLE `user_verification_code`
     `id`              BIGINT NOT NULL AUTO_INCREMENT,
     `user_id`         BIGINT NOT NULL,
     `email_code`      VARCHAR(20),
-    `fcm_token`       VARCHAR(50),
+    `fcm_token`       VARCHAR(150),
     `expiration_date` DATETIME,
     PRIMARY KEY (`id`)
 );
